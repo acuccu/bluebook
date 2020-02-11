2b98d0a68c8ea61c52a86925cc99ed0651a62c35
Antonio <cuccu.a@gmail.com>
2020-02-11 17:23:50 -0500

    store, entry, and root complete

--- frontend/bluebook.jsx
+++ frontend/bluebook.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from './components/Root'
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
   ReactDOM.render(<h1>The Bluebook</h1>, root);
 });
