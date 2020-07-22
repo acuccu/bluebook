c2e6b7e44549078f5bd102e4798b1891b1a8a905
Antonio <cuccu.a@gmail.com>
2020-07-22 03:40:52 -0400

    added first friend on registration

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,10 +12,11 @@ class Api::UsersController < ApplicationController
     def create 
         
         @user = User.new(user_params)
-        @friendships = []
+        first_friendship = Friendship.create({user_id: @user.id, friend_id: 1, accepted: true})
+        @friendships = [first_friendship]
         @pending = []
-        @accepted = []
-        @friends = []
+        @accepted = [first_friendship]
+        @friends = [first_friendship.friend]
         if @user.save
             log_in!(@user)
             render :show
