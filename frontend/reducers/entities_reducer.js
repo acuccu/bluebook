12efe6330e58b7eb1588f3a31b8ed7d0659faabb
Antonio <cuccu.a@gmail.com>
2021-07-17 10:04:37 +0200

    reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,14 @@
 import {combineReducers} from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
-import friendsReducer from './friends_reducer'
+import friendsReducer from './friends_reducer';
+import likesReducer from './likes_reducer'; 
 
-const entitiesReducer = combineReducers({users: usersReducer, posts: postsReducer, friends: friendsReducer});
+const entitiesReducer = combineReducers(
+    {   users: usersReducer, 
+        posts: postsReducer, 
+        friends: friendsReducer,
+        likes: likesReducer
+    });
 
 export default entitiesReducer
